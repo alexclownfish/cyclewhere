8ab1f6e091ea7b6274ea449979881cc087de96c2
alexclownfish <ywz0207@163.com>
2026-08-07 22:58:26 +0800

    test: support race checks on legacy Docker

--- scripts/go-race-test.sh
+++ scripts/go-race-test.sh
@@ -2,8 +2,11 @@
 set -eu
 
 docker run --rm \
+  --security-opt seccomp=unconfined \
   -e GOPROXY=https://goproxy.cn,direct \
   -v /opt/fengji/apps/api-go:/src \
+  -v cyclewhere-go-race-mod:/go/pkg/mod \
+  -v cyclewhere-go-race-build:/root/.cache/go-build \
   -w /src \
   golang:1.25-bookworm \
   go test -race ./...
